09f4e9b73b75636fb057be74aa2a3b61f0969289
wnowers <89948685+wnowers@users.noreply.github.com>
2021-09-29 21:30:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+// part 1
+function computeForce() {
+    let mass = parseFloat(document.getElementById('mass').value);
+    let accel = parseFloat(document.getElementById('accel').value);
+    let force = mass * accel;
+    document.getElementById('result').innerHTML = force;
+}
+
+
+// part 2
+function computeAvgVelocity() {
+    // code
+
+}
+
+
+// part 3
+function computePrincipal() {
+    // code
+
+}
+
+// part 4
+function computePresentValue() {
+    // code
+
+}
