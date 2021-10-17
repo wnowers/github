54d076357f524988fb827ed63e6efe129e62abc8
wnowers <89948685+wnowers@users.noreply.github.com>
2021-10-17 18:13:52 -0400

    ITEC-225 Proj.2
    Included is an Index page, CSS, and JavaScript for project 2

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
+var mc2c = {'.-': 'A', '-...': 'B', '-.-.': 'C',
+            '-..': 'D', '.': 'E', '..-.': 'F',
+            '--.': 'G', '....': 'H', '..': 'I',  
+            '.---': 'J', '-.-': 'K', '.-..': 'L',
+            '--': 'M', '-.': 'N', '---': 'O', 
+            '.--.': 'P', '--.-': 'Q', '.-.': 'R',
+            '...': 'S', '-': 'T', '..-': 'U', 
+            '...-': 'V', '.--': 'W', '-..-': 'X',
+            '-.--': 'Y', '--..': 'Z', '-----': '0', 
+            '.----': '1', '..---': '2', '...--': '3',
+            '....-': '4', '.....': '5', '-....': '6', 
+            '--...': '7', '---..': '8', '----.': '9',
+            '--..--':',', '.-.-.-':'.', '..--..':'?',
+            '-..-.':'/', '-....-':'-', '-.--.':'(',
+            '-.--.-':')', '|':' ', ' ':''};
 
+var c2mc = { 'A':'.-', 'B':'-...', 
+              'C':'-.-.' , 'D':'-..', 'E':'.', 
+              'F':'..-.', 'G':'--.', 'H':'....', 
+              'I':'..', 'J':'.---', 'K':'-.-', 
+              'L':'.-..', 'M':'--', 'N':'-.', 
+              'O':'---', 'P':'.--.', 'Q':'--.-', 
+              'R':'.-.', 'S':'...', 'T':'-', 
+              'U':'..-', 'V':'...-', 'W':'.--', 
+              'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+              '1':'.----', '2':'..---', '3':'...--', 
+              '4':'....-', '5':'.....', '6':'-....', 
+              '7':'--...', '8':'---..', '9':'----.', 
+              '0':'-----', ',':'--..--', '.':'.-.-.-', 
+              '?':'..--..', '/':'-..-.', '-':'-....-', 
+              '(':'-.--.', ')':'-.--.-', ' ':'|'};
 
-
-// part 1
-function computeForce() {
-    let mass = parseFloat(document.getElementById('mass').value);
-    let accel = parseFloat(document.getElementById('accel').value);
-    let force = mass * accel;
-    document.getElementById('result').innerHTML = force;
- }
-
-
-// part 2
-function computeAvgVelocity() {
-    let velo = parseFloat(document.getElementById('initialVelo').value);
-    let time = parseFloat(document.getElementById('timeInSeconds').value);
-    let accel = parseFloat(document.getElementById('acceler').value);
-    let displ = (velo * time) - (accel * Math.pow(time, 2));
-    document.getElementById('result2').innerHTML = displ.toString();
-
-}
-
-
-// part 3
-function computePrincipal() {
-    let sideA = prompt("Enter first number", "0");
-         sideA = parseFloat(sideA);
-    let sideB = prompt("Enter second number", "0");
-        sideB = parseFloat(sideB);
-    let sideC = prompt("Enter thirds number", "0");
-        sideC = parseFloat(sideC);
-    
-    let discrim = (Math.pow(sideB, 2) - 4 * sideA * sideC);
-    
-    if (discrim > 0){
-        firstTri = (-1 * sideB + Math.sqrt(discrim))/(2 * sideA);
-        secondTri = (-1 * sideB - Math.sqrt(discrim))/(2 * sideA);
-    } else if (discrim < 0){
-        firstTri = (-1 * sideB + Math.sqrt(discrim * -1))/ (2 * sideA);
-        secondTri = (-1 * firstTri);
-    
-        document.getElementById('result3').innerHTML = firstTri.toString();
-    document.getElementById('result4').innerHTML = secondTri.toString();
+    function toMorse(){
+        var text = document.getElementById("text").value.toUpperCase();
+        var ans = "";
+        for(let i = 0; i < text.length; i++){
+            var key = text.charAt(i);
+            for(var lett in c2mc){
+                if (key == lett){
+                    ans += c2mc[lett] + " ";
+                }
+            }
+        }
+        document.getElementsByName("morse")[0].value = ans;
     }
-}
 
-// part 4
-function computePresentValue() {
-    let payment = prompt("Enter annuity payment", "0");
-        payment = parseFloat(payment);
-    let rate = prompt("Enter interest rate", "0");
-        rate = parseFloat(rate);
-    let period = prompt("Enter number of periods", "0");
-        period = parseFloat(period);
-    let value = payment * 1 - (1/Math.pow(1 + rate, period)/rate);
-
-    document.getElementById('result5').innerHTML = value.toString();
-
-
-
-}
+    function toText(){
+        var text = document.getElementById("text").value.toUpperCase();
+        var ansTwo = "";
+        var split = text.split(' ');
+        for(let i = 0; i < split.length; i++){
+            var key = split[i];
+            for(var code in mc2c){
+                if (key == code){
+                    ansTwo += mc2c[code];
+                }
+            }
+        }
+        document.getElementsByName("morse")[0].value = ansTwo;
+    }
